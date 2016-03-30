affd293817b35b36c9f1e58d8871b3293e168e97
Francisco Reyes <francisco@makingdevs.com>
2016-03-30 17:37:57 -0600

    Se agrega boton para eliminar un solo archivo

--- js/Dropzone.js
+++ js/Dropzone.js
@@ -11,31 +11,37 @@ Dropzone.options.uploadWidget = {
       console.log( resp );
     });
 */
-    var submitButton = document.querySelector("#submit")
-    myDropzone = this; // closure
-
-    
+var submitButton = document.querySelector("#submit")
+    myDropzone = this; 
     submitButton.addEventListener("click", function(e){
-    	e.preventDefault();
-    	e.stopPropagation();
     	myDropzone.processQueue();
     });
+
     this.on("addedfile", function(file, resp ) {
     	console.log( file );
     	console.log( resp );
+      var removeButton = Dropzone.createElement("<button>Remove file</button>");
+      
+      removeButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        myDropzone.removeFile(file);
+      });
+      file.previewElement.appendChild(removeButton);
+
     });
 
     this.on("complete", function(file) {
-    	//myDropzone.removeFile(file);
+    	
     });
 
     this.on("success", 
     	myDropzone.processQueue.bind(myDropzone)
-    	);
+    );
 
     var cleanButton=document.querySelector("button#clear")
     cleanButton.addEventListener("click", function(e) {
-        myDropzone.removeAllFiles();
+      myDropzone.removeAllFiles();
     });
 
   }
